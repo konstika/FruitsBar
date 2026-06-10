ff472f7d4c93b5dcd067b64120f5a5e76a99df9e
Yarikosk <yarikom2005@gmail.com~>
2026-06-10 22:37:07 +0300

    Update GuestsQueueController.cs

--- Assets/Scripts/Guest/GuestsQueueController.cs
+++ Assets/Scripts/Guest/GuestsQueueController.cs
@@ -11,32 +11,42 @@ public class GuestsQueueController : MonoBehaviour
     {
         _queue = new Guest[]{ 
             new Guest(
-                new string[]{"Hello, is this really fruit? Is it the result of some new genetic technology?", "I want a fruit... lemon"},
-                new string[]{"Eerr? What is it?", "Apparently, the tastes used to be completely different...", ""},
-                new string[] {"Wow, it's delicious", "Thank you",""},
+                new string[]{"Hello!", "Are that really 'Fruits' in your bar?", "I thought they were lost long time ago, when humanity entered the era of cyberspace...", "I want to try some of that... like lemon"},
+                new string[]{"Ew? What's that?", "I wonder if way back tastes used to be completely different...", ""},
+                new string[] { "Wow, it's delicious! Thank you!",""},
                 new Ingridient[]{ Ingridient.Lemon, Ingridient.Sugar },
                 new Ingridient[]{Ingridient.Lemon, Ingridient.Sugar, Ingridient.Water, Ingridient.Ice }
             ),
+
             new Guest(
-                new string[]{ "What an interesting place", "I've heard from some of my extremely ancient friends that there were less accessible tropical fruits", "By the way, those red fruits look unusual, almost tropical"},
-                new string[]{ "Oh, to be honest, I feel disappointed", "I don't know where you came from, but I want to give you some advice as a successful businesswoman", "Catering to customers is an outdated strategy", "People appreciate the idea and the truth more",""},
-                new string[] { "Wow, haha, I didn't choose the right color", "It's really a very harmonious drink. Tropical...", "I'll be back, and good luck with your business",""},
+                new string[]{ "What an interesting place", "I've heard from some of my rusty old friends that there used to be more inaccessible tropical fruits and from which very tasty cocktails were made ", "By the way, this round red fruit looks unusual, almost tropical"},
+                new string[]{ "To be honest, I feel disappointed.", "I don't know where are you from, but I want to give you some advice as a successful businesswoman", "Catering to customers is an outdated strategy", "People appreciate the idea and the truth more",""},
+                new string[] { "Wow, haha, I didn't choose the right color", "It's really a very harmonious blend of flavours. Tropical...", "One day I will return. Good luck with your business!",""},
                 new Ingridient[]{ Ingridient.Pineapple, Ingridient.Mango },
-                new Ingridient[]{ Ingridient.Pineapple, Ingridient.Mango, Ingridient.Water, Ingridient.Ice, Ingridient.Lemon }
+                new Ingridient[]{ Ingridient.Pineapple, Ingridient.Mango, Ingridient.Water, Ingridient.Ice }
             ),
+
             new Guest(
-                new string[]{ "Hi, I see they sell antiquities here", "I want to taste ancient blood, ancient blue blood"},
-                new string[]{ "It doesn't look like blood", "Bye",""},
-                new string[] { "It looks like blood", "It is interesting",""},
+                new string[]{ "Hi, I see they sell antiquities here", "I want to taste ancient blood, ancient blue blood!"},
+                new string[]{ "It doesn't seems like blood", "Bye",""},
+                new string[]{ "It seems like blood", "Interesting...",""},
                 new Ingridient[]{ Ingridient.Tomato, Ingridient.Salt },
                 new Ingridient[]{ Ingridient.Tomato, Ingridient.Salt }
             ),
+
             new Guest(
-                new string[]{ "Hi, I have a pet, and it's organic too. They say he looks like an ancient cat or dog", "But he's much more sensitive to food. It's hot, and he's very thirsty. Maybe organic food wouldn't hurt him?"},
+                new string[]{ "Hi, I have a pet, and it's organic too. My parents said that it's like ancient pets — you know, cats, dogs, that kind of thing.", "But he's much more sensitive to food. It's very hot today, and he's very thirsty. Maybe some organic drink wouldn't hurt?"},
                 new string[]{ "I hope it doesn't hurt him",""},
                 new string[] { "I hope it doesn't hurt him",""},
                 new Ingridient[]{ Ingridient.Water },
                 new Ingridient[]{ Ingridient.Water, Ingridient.Ice }
+            ),
+            new Guest(
+                new string[]{ "And here we have another strange cafe!","Look at all this weird things on shelves, that they call 'Fruits'!","I wonder if these are just cheap, nasty substitutes like they feed us in all those other places.","Hey, you here! Make me a drink with that little red whatever-it-is.", "I bet it will be as sour, bittery and burning spicy as everywhere.", ""},
+                new string[]{ "I knew that would happen! And they actually have the nerve to serve this crap to customers.", ""},
+                new string[] { "Wow, now this is a pleasant drink — never had one like it. Not like in those other bars.", "Thanks!",""},
+                new Ingridient[]{ Ingridient.Strawberry, Ingridient.Sugar },
+                new Ingridient[]{ Ingridient.Water, Ingridient.Ice, Ingridient.Strawberry, Ingridient.Sugar }
             )
         };
         _guestController = _guest.GetComponent<GuestController>();
